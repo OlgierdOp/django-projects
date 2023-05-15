00b1bba41b796fc2452f9059142c14f7206c92ed
OlgierdOp <you@example.com>
2023-05-15 18:01:51 +0200

    nothing

--- crm/user_communication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import send_message
-
-app_name = "user_communication"
-
-urlpatterns = [
-    path('', send_message),
-]
--- crm/user_communication/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def send_message(request):
-    return render(request, "user_communication/index.html")
-
